d2d2e57aafb67b8390c5aeb9bf5e550c08cee485
Jess Whitworth <jessewhitworth@gmail.com>
2016-06-23 23:17:03 -0400

    Update MainServlet.java

--- src/main/java/net/aplayfullife/identity/MainServlet.java
+++ src/main/java/net/aplayfullife/identity/MainServlet.java
@@ -4,7 +4,7 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import java.util.list;
+import org.apache.commons.io.IOUtils;
 
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
@@ -12,13 +12,11 @@ public class MainServlet extends HttpServlet {
       throws ServletException, IOException {
     ServletContext context = getServletContext();
     InputStream resourceContent = context.getResourceAsStream("/resources/templates/identity_main.html");
-    String template = "";
-    byte input = resourceContent.read();
-    while (input != -1) {
-      template = template.concat(input.toChar());
-      input = resourceContent.read();
-    }
+    StringWriter writer = new StringWriter();
+    IOUtils.copy(inputStream, writer, "UTF-8");
+    String template = writer.toString();
+    IOUtils.closeQuietly(inputStream);
     response.setContentType("text/html; charset=UTF-8");
-    response.getOutputStream().print("<html><body>This was written by a servlet. Amazing, no?</body></html>");
+    response.getOutputStream().print(template);
   }
 }
