180e306b2f68c37abeb16acc07bacc5913ef51ad
Jess Whitworth <jessewhitworth@gmail.com>
2016-07-05 17:53:49 -0400

    Create MainServlet.java

---
+++ src/main/java/net/aplayfullife/home/MainServlet.java
@@ -0,0 +1,27 @@
+package net.aplayfullife.home;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import org.apache.commons.io.IOUtils;
+
+@WebServlet("")
+public class MainServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    ServletContext context = getServletContext();
+    InputStream resourceContent = context.getResourceAsStream("/WEB-INF/templates/home_main.html");
+    StringWriter writer = new StringWriter();
+    IOUtils.copy(resourceContent, writer, "UTF-8");
+    String template = writer.toString();
+    IOUtils.closeQuietly(resourceContent);
+    resourceContent = context.getResourceAsStream("/WEB-INF/templates/home_header.html");
+    writer.getBuffer().setLength(0);
+    IOUtils.copy(resourceContent, writer, "UTF-8");
+    template = template.replace("{site_header}", writer.toString());
+    IOUtils.closeQuietly(resourceContent);
+    response.setContentType("text/html; charset=UTF-8");
+    response.getOutputStream().print(template);
+  }
+}
