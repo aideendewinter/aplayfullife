45bd76c92b579a8223752c5154cc10104a72cdb8
Jess Whitworth <jessewhitworth@gmail.com>
2016-07-05 20:02:02 -0400

    Update MainServlet.java

--- src/main/java/net/aplayfullife/home/MainServlet.java
+++ src/main/java/net/aplayfullife/home/MainServlet.java
@@ -16,11 +16,19 @@ public class MainServlet extends HttpServlet {
     IOUtils.copy(resourceContent, writer, "UTF-8");
     String template = writer.toString();
     IOUtils.closeQuietly(resourceContent);
+    // Header
     resourceContent = context.getResourceAsStream("/WEB-INF/templates/home_header.html");
     writer.getBuffer().setLength(0);
     IOUtils.copy(resourceContent, writer, "UTF-8");
     template = template.replace("{site_header}", writer.toString());
     IOUtils.closeQuietly(resourceContent);
+    // Page Content
+    resourceContent = context.getResourceAsStream("/WEB-INF/content/home_main");
+    writer.getBuffer().setLength(0);
+    IOUtils.copy(resourceContent, writer, "UTF-8");
+    template = template.replace("{page_content}", writer.toString());
+    IOUtils.closeQuietly(resourceContent);
+    // Output
     response.setContentType("text/html; charset=UTF-8");
     response.getOutputStream().print(template);
   }
