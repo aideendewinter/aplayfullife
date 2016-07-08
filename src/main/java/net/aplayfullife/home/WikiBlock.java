ea0ce61a32632ee910b70def67dc519b3e67f636
Jess Whitworth <jessewhitworth@gmail.com>
2016-07-07 22:40:45 -0400

    Create WikiBlock.java

---
+++ src/main/java/net/aplayfullife/home/WikiBlock.java
@@ -0,0 +1,15 @@
+package net.aplayfullife.home;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import org.apache.commons.io.IOUtils;
+
+public class WikiBlock implements ContentBlock {
+  public WikiBlock(String URL) {
+    
+  }
+  public String GetHTML() {
+  }
+}
