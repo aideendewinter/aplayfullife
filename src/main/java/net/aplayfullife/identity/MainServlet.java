dfd4a3a0ab5a8e545761e38779ba1ba632da23ff
Jess Whitworth <jessewhitworth@gmail.com>
2016-07-25 19:49:48 -0400

    Update MainServlet.java

--- src/main/java/net/aplayfullife/identity/MainServlet.java
+++ src/main/java/net/aplayfullife/identity/MainServlet.java
@@ -1,6 +1,7 @@
 package net.aplayfullife.identity;
 
 import java.io.*;
+import java.util.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +11,76 @@ import org.apache.commons.io.IOUtils;
 public class MainServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    ServletContext context = getServletContext();
-    InputStream resourceContent = context.getResourceAsStream("/WEB-INF/templates/identity_main.html");
+    IdentityTemplate template = new IdentityTemplate("home_main", this);
+    
+    InputStream resourceContent;
     StringWriter writer = new StringWriter();
-    IOUtils.copy(resourceContent, writer, "UTF-8");
-    String template = writer.toString();
-    IOUtils.closeQuietly(resourceContent);
-    resourceContent = context.getResourceAsStream("/WEB-INF/templates/identity_header.html");
+    // Page Navigation
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    resourceContent = classLoader.getResourceAsStream("/content/home_pages");
     writer.getBuffer().setLength(0);
     IOUtils.copy(resourceContent, writer, "UTF-8");
-    template = template.replace("{site_header}", writer.toString());
+    ArrayList<String> pages = new ArrayList<String>(Arrays.asList(writer.toString().split("\\r?\\n")));
     IOUtils.closeQuietly(resourceContent);
+    String pageNav = "";
+    for (String page : pages) {
+      String[] pageInfo = page.split(",");
+      pageNav += "<a href=\"./" + pageInfo[1] + ".html\">" + pageInfo[0] + "</a>";
+    }
+    template.SetPageNavigation(pageNav);
+    // Page Content
+    String path = request.getServletPath().replace(".html", "");
+    if (path == "")
+      resourceContent = classLoader.getResourceAsStream("/content/home_main");
+    else
+      resourceContent = classLoader.getResourceAsStream("/content" + path);
+    writer.getBuffer().setLength(0);
+    IOUtils.copy(resourceContent, writer, "UTF-8");
+    ArrayList<String> blockIds = new ArrayList<String>(Arrays.asList(writer.toString().split(",")));
+    IOUtils.closeQuietly(resourceContent);
+    template.SetPageHeader(blockIds.remove(0));
+    String content;
+    if (path == "") {
+      content = parseBlocks("/home", blockIds);
+    }
+    else {
+      content = parseBlocks(path, blockIds);
+    }
+    template.SetPageContent(content);
+    // Output
     response.setContentType("text/html; charset=UTF-8");
-    response.getOutputStream().print(template);
+    response.getWriter().print(template.GetPage());
+  }
+  
+  protected String parseBlocks(String page, List<String> blockIds) {
+    String blockOutput = "";
+    for(String blockId : blockIds) {
+      blockId = blockId.trim();
+      ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+      InputStream resourceContent = classLoader.getResourceAsStream("/content"+page+"-blocks/" + blockId);
+      StringWriter writer = new StringWriter();
+      try {
+        IOUtils.copy(resourceContent, writer, "UTF-8");
+        String blockContent = writer.toString();
+        if (blockId.contains("text")) {
+          blockOutput += "<p class=\"block text\">" + blockContent + "</p>";
+        } else if (blockId.contains("wiki")) {
+          try {
+            WikiBlock wBlock = new WikiBlock(blockContent);
+            blockOutput += "<p class=\"block wiki\">" + wBlock.GetHTML() + "</p>";
+          }
+          catch (IOException e) {
+            blockOutput += "<h1>Bad Wiki Block<h1>";
+          }
+        } else if (blockId.contains("html")) {
+          blockOutput += "<div class=\"block html\">" + blockContent + "</div>";
+        }
+      }
+      catch (IOException | java.lang.NullPointerException ex) {
+        blockOutput += "Bad Block : " + blockId + ".";
+      }
+      IOUtils.closeQuietly(resourceContent);
+    }
+    return blockOutput;
   }
 }
