6204325a2c7cf8cdc09f68ea61829fc9d6ceb7eb
Jess Whitworth <jessewhitworth@gmail.com>
2016-07-29 20:07:59 -0400

    Update MindTextBlock.java

--- src/main/java/net/aplayfullife/identity/MindTextBlock.java
+++ src/main/java/net/aplayfullife/identity/MindTextBlock.java
@@ -8,17 +8,19 @@ import javax.servlet.annotation.WebServlet;
 import org.apache.commons.io.IOUtils;
 
 public class MindTextBlock implements ContentBlock {
-  String text;
-  public MindTextBlock(String text) {
-    this.text = text;
-  }
-  public String GetHTML() throws IOException {
-    ArrayList<String> paragraphs = new ArrayList<String>(Arrays.asList(writer.toString().split("\\r?\\n\\r?\\n")));
-    String parsed = "";
+	String text;
+	
+	public MindTextBlock(String text) {
+		this.text = text;
+	}
+	
+	public String GetHTML() throws IOException {
+		ArrayList<String> paragraphs = new ArrayList<String>(Arrays.asList(text.split("\\r?\\n\\r?\\n")));
+		String parsed = "";
 		for (String paragraph : paragraphs) {
 			parsed += "<p>" + paragraph + "</p>";
 		}
 		
 		return parsed;
-  }
+	}
 }
