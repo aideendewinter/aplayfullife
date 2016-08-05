bbaba68dd458abd7befd5424ecce2842336971e4
Jess Whitworth <jessewhitworth@gmail.com>
2016-08-04 20:46:04 -0400

    Create MindMap.java

---
+++ src/main/java/net/aplayfullife/identity/MindMap.java
@@ -0,0 +1,25 @@
+package net.aplayfullife.identity;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import org.apache.commons.io.IOUtils;
+
+public class MindMap {
+	
+	public MindMap(String directory, IdentityTemplate template) {
+		// Content readers.
+		InputStream resourceContent;
+		StringWriter writer = new StringWriter();
+		
+		// 
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		resourceContent = classLoader.getResourceAsStream("/content/identity-pages");
+		writer.getBuffer().setLength(0);
+		IOUtils.copy(resourceContent, writer, "UTF-8");
+		template.DebugPrintLn(writer.toString());
+		IOUtils.closeQuietly(resourceContent);
+	}
+}
