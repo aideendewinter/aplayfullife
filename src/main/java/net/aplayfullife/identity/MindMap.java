90f4375b3a9fce4ee69128454de7a1d0f947c084
Jess Whitworth <jessewhitworth@gmail.com>
2016-08-05 23:22:02 -0400

    Update MindMap.java

--- src/main/java/net/aplayfullife/identity/MindMap.java
+++ src/main/java/net/aplayfullife/identity/MindMap.java
@@ -20,12 +20,31 @@ public class MindMap {
 		writer.getBuffer().setLength(0);
 		IOUtils.copy(resourceContent, writer, "UTF-8");
 		String[] pages = writer.toString().split("\\r?\\n");
+		IOUtils.closeQuietly(resourceContent);
 		for(int i=0; i<pages.length; i++) {
 			resourceContent = classLoader.getResourceAsStream("/content/identity-pages/" + pages[i]);
 			writer.getBuffer().setLength(0);
 			IOUtils.copy(resourceContent, writer, "UTF-8");
-			template.DebugPrintLn(i + writer.toString());
+			ArrayList<String> blockIds = new ArrayList<String>(Arrays.asList(writer.toString().split(",")));
+			IOUtils.closeQuietly(resourceContent);
+			blockIds.remove(0);
+			
+			for(String blockId : blockIds) {
+				blockId = blockId.trim();
+				resourceContent = classLoader.getResourceAsStream("/content/identity-blocks/" + blockId);
+				writer.getBuffer().setLength(0);
+				
+				try {
+					IOUtils.copy(resourceContent, writer, "UTF-8");
+					String blockContent = writer.toString();
+					if (blockId.contains("mindtext")) {
+						
+					}
+				} catch (IOException | java.lang.NullPointerException ex) {
+					content += "Bad Block : " + blockId + ".";
+				}
+				IOUtils.closeQuietly(resourceContent);
+			}
 		}
-		IOUtils.closeQuietly(resourceContent);
 	}
 }
