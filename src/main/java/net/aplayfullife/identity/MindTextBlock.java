3f5a43c1737fef405da8f56c2a470a3c5d8a24c6
jesswhitworth <jessewhitworth@gmail.com>
2016-08-09 01:26:52 -0400

    Forgot java caps String.

--- src/main/java/net/aplayfullife/identity/MindTextBlock.java
+++ src/main/java/net/aplayfullife/identity/MindTextBlock.java
@@ -23,7 +23,7 @@ public class MindTextBlock {
 		for (int i=0; i<mindMap.mapMatrix[pageIndex].length; i++) {
 			if (i==pageIndex)
 				continue;
-			string linkWeight="normal-weight";
+			String linkWeight="normal-weight";
 			mapped = mapped.replaceFirst(mindMap.pages[i], "<a class=" + linkWeight + "href=\"/identity/" + mindMap.pages[i] + ".html\">" 
 				+ mindMap.pages[i] + "("+mindMap.mapMatrix[pageIndex][i]+" "+mindMap.mapMatrix[i][pageIndex]+")</a>");
 		}
