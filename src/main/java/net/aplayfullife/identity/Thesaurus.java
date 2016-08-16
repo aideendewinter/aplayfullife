a641dd8b327aca00f6fc00d76ca5df9887f1b68b
Jess Whitworth <jessewhitworth@gmail.com>
2016-08-16 18:22:26 -0400

    Update Thesaurus.java

--- src/main/java/net/aplayfullife/identity/Thesaurus.java
+++ src/main/java/net/aplayfullife/identity/Thesaurus.java
@@ -9,35 +9,36 @@ import org.json.simple.*;
 public class Thesaurus { 
   final String endpoint = "http://thesaurus.altervista.org/thesaurus/v1"; 
 
-  public JSONArray SendRequest(String word) { 
-    String language = "en_US";
-    String output = "json";
-    String key = "VmMAF4xvnkWFt1IFwMBs";
-    try { 
-      URL serverAddress = new URL(endpoint + "?word="+URLEncoder.encode(word, "UTF-8")+"&language="+language+"&key="+key+"&output="+output); 
-      HttpURLConnection connection = (HttpURLConnection)serverAddress.openConnection(); 
-      connection.connect(); 
-      int rc = connection.getResponseCode(); 
-      if (rc == 200) { 
-        String line = null; 
-        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(connection.getInputStream())); 
-        StringBuilder sb = new StringBuilder(); 
-        while ((line = br.readLine()) != null) 
-          sb.append(line + '\n'); 
-        JSONObject obj = (JSONObject) JSONValue.parse(sb.toString()); 
-        JSONArray array = (JSONArray)obj.get("response");
-		return array;
-      } else System.out.println("HTTP error:"+rc); 
-      connection.disconnect(); 
-    } catch (java.net.MalformedURLException e) { 
-      e.printStackTrace(); 
-    } catch (java.net.ProtocolException e) { 
-      e.printStackTrace(); 
-    } catch (java.io.IOException e) { 
-      e.printStackTrace(); 
-    }
-
-	return null;
-	
-  } 
+	public JSONArray SendRequest(String word) { 
+		String language = "en_US";
+		String output = "json";
+		String key = "VmMAF4xvnkWFt1IFwMBs";
+		try { 
+			URL serverAddress = new URL(endpoint + "?word="+URLEncoder.encode(word, "UTF-8")+"&language="+language+"&key="+key+"&output="+output); 
+			HttpURLConnection connection = (HttpURLConnection)serverAddress.openConnection(); 
+			connection.connect(); 
+			int rc = connection.getResponseCode(); 
+			if (rc == 200) { 
+				String line = null; 
+				BufferedReader br = new BufferedReader(new java.io.InputStreamReader(connection.getInputStream())); 
+				StringBuilder sb = new StringBuilder(); 
+        	
+        			while ((line = br.readLine()) != null) 
+					sb.append(line + '\n'); 
+		
+				JSONObject obj = (JSONObject) JSONValue.parse(sb.toString()); 
+				JSONArray array = (JSONArray)obj.get("response");
+		
+				return array;
+			} else System.out.println("HTTP error:"+rc); 
+			connection.disconnect(); 
+		} catch (java.net.MalformedURLException e) { 
+			e.printStackTrace(); 
+		} catch (java.net.ProtocolException e) { 
+			e.printStackTrace(); 
+		} catch (java.io.IOException e) { 
+			e.printStackTrace(); 
+		}
+		return null;
+	} 
 }
