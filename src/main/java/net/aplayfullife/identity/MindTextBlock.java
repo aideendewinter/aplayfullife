a642ec75c337d75d4e80a09f2c4f4361be6ee86e
jesswhitworth <jessewhitworth@gmail.com>
2016-08-27 00:48:49 -0400

    Forgot to escape my escapes.

--- src/main/java/net/aplayfullife/identity/MindTextBlock.java
+++ src/main/java/net/aplayfullife/identity/MindTextBlock.java
@@ -39,7 +39,7 @@ public class MindTextBlock {
 				}
 			}
 		}
-		mapped.replaceAll("[\[\]]", "")
+		mapped.replaceAll("[\\[\\]]", "")
 		ArrayList<String> paragraphs = new ArrayList<String>(Arrays.asList(mapped.split("\\r?\\n\\r?\\n")));
 		String parsed = "";
 		for (String paragraph : paragraphs) {
