e3555c331e8ab47b4facbd25435968d2ee57e647
Jess Whitworth <jessewhitworth@gmail.com>
2016-09-01 18:23:37 -0400

    Update UnityProgress.js

--- src/main/webapp/webgl/fish/TemplateData/UnityProgress.js
+++ src/main/webapp/webgl/fish/TemplateData/UnityProgress.js
@@ -12,19 +12,19 @@ function UnityProgress (dom) {
 	this.background = background;
 
 	var logoImage = document.createElement("img");
-	logoImage.src = "TemplateData/progresslogo.png"; 
+	logoImage.src = "/webgl/fish/TemplateData/progresslogo.png"; 
 	logoImage.style.position = "absolute";
 	parent.appendChild(logoImage);
 	this.logoImage = logoImage;
 
 	var progressFrame = document.createElement("img");
-	progressFrame.src = "TemplateData/loadingbar.png"; 
+	progressFrame.src = "/webgl/fish/TemplateData/loadingbar.png"; 
 	progressFrame.style.position = "absolute";
 	parent.appendChild(progressFrame);
 	this.progressFrame = progressFrame;
 
 	var progressBar = document.createElement("img");
-	progressBar.src = "TemplateData/fullbar.png"; 
+	progressBar.src = "/webgl/fish/TemplateData/fullbar.png"; 
 	progressBar.style.position = "absolute";
 	parent.appendChild(progressBar);
 	this.progressBar = progressBar;
