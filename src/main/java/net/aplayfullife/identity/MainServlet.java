b912c4a29c2577e2608a97f87bfedec681997a63
jesswhitworth <jessewhitworth@gmail.com>
2016-09-03 06:14:00 -0400

    Handle non-trailing-slash servlet matches. Maybe.

--- src/main/java/net/aplayfullife/identity/MainServlet.java
+++ src/main/java/net/aplayfullife/identity/MainServlet.java
@@ -36,7 +36,7 @@ public class MainServlet extends HttpServlet {
 		
 		// Page Content
 		String path = request.getPathInfo().replace(".html", "");
-		if (path.equals("/")) {
+		if (path.equals("/") || path.equals("")) {
 			resourceContent = classLoader.getResourceAsStream("/content/identity_main");
 		} else
 			resourceContent = classLoader.getResourceAsStream("/content/identity-pages" + path);
@@ -46,7 +46,7 @@ public class MainServlet extends HttpServlet {
 		ArrayList<String> blockIds = new ArrayList<String>(Arrays.asList(writer.toString().split(",")));
 		IOUtils.closeQuietly(resourceContent);
 		template.SetPageHeader(blockIds.remove(0));
-		if (path.equals("/")) {
+		if (path.equals("/") || path.equals("")) {
 			parseBlocks("identity", blockIds, template, mindMap);
 		} else {
 			parseBlocks(path.replace("/", ""), blockIds, template, mindMap);
